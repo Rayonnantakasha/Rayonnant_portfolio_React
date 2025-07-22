ea49a9972745742eb894d1e60f646177590ca436
Rayonnant akasha <nyonsaakasha@gmail.com>
2025-07-22 15:42:16 +0100

    Feat : Create Header componant

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import logo from "../assets/logo.svg"
+
+function Header() {
+    return (
+    <header className="fixed top-0 left-0 w-full z-10 bg-[#2D383C] flex items-center justify-between lg:px-32 px-2 py-2" id="header">
+      <div>
+        <a href=""><img src={logo} alt="akashadev" className="lg:w-52 w-36" /></a>
+      </div>
+      <nav className="sm:flex justify-between gap-4 items-center">
+          <a href="#accueil" className="hover:text-[#ff5940] font-medium transition duration-300 ease-in-out text-base">Accueil</a>
+          <a href="#competences" className="hover:text-[#ff5940] font-medium transition duration-300 ease-in-out text-base">Compétences</a>
+          <a href="#apropos" className="hover:text-[#ff5940] font-medium transition duration-300 ease-in-out text-base">À propos</a>
+      </nav>
+    </header>
+    )
+}
+
+export default Header
