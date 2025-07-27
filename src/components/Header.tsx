f4019892866e25db15c94eee47fbb0c90978cd71
Rayonnant akasha <nyonsaakasha@gmail.com>
2025-07-27 18:51:13 +0100

    Feat: add Projet link on menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,13 @@ function Header() {
     return (
     <header className="fixed top-0 left-0 w-full z-10 bg-[#2D383C] flex items-center justify-between lg:px-32 px-2 py-2" id="header">
       <div>
-        <a href=""><img src={logo} alt="akashadev" className="lg:w-52 w-36" /></a>
+        <a href="/"><img src={logo} alt="akashadev" className="lg:w-52 w-36" /></a>
       </div>
       <nav className="sm:flex justify-between gap-4 items-center">
-          <a href="#accueil" className="hover:text-[#ff5940] font-medium transition duration-300 ease-in-out text-base">Accueil</a>
+          <a href="/" className="hover:text-[#ff5940] font-medium transition duration-300 ease-in-out text-base">Accueil</a>
           <a href="#competences" className="hover:text-[#ff5940] font-medium transition duration-300 ease-in-out text-base">Compétences</a>
           <a href="#apropos" className="hover:text-[#ff5940] font-medium transition duration-300 ease-in-out text-base">À propos</a>
+          <a href="/MesProjets" className="hover:text-[#ff5940] font-medium transition duration-300 ease-in-out text-base">Projets</a>
       </nav>
     </header>
     )
