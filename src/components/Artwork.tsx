f780c9bcea24217f6928630caf7aee540982dcef
Rayonnant akasha <nyonsaakasha@gmail.com>
2025-07-27 18:49:51 +0100

    Breaking Change: Add root for Project page

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -8,7 +8,7 @@ function Artwork() {
                <h2 className="lg:text-8xl text-6xl font-black text-outline-white">
                 Envie de <br />découvrir mes <br /> œuvres ? </h2>
                <div className="pt-12">
-                  <a href="/projets" className="bg-[#ff5940] p-4 hover:bg-orange-800 transition duration-300 ease-in-out">
+                  <a href="/MesProjets" className="bg-[#ff5940] p-4 hover:bg-orange-800 transition duration-300 ease-in-out">
                      <span className="font-bold">CLIQUEZ ICI</span>
                   </a>
                </div>
