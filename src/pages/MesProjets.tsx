4b5ee6570d2c206087efb86c47c1c3fc427e394e
Rayonnant akasha <nyonsaakasha@gmail.com>
2025-07-27 18:48:54 +0100

    Feat : Create Project pages

---
+++ src/pages/MesProjets.tsx
@@ -0,0 +1,10 @@
+
+function MesProjets() {
+    return (
+        <section id="projets">
+            <h2 className="font-extrabold text-center text-2xl lg:text-6xl">MES PROJETS</h2>
+        </section>
+    )
+}
+
+export default MesProjets
