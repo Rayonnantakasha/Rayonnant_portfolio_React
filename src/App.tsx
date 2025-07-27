e4bbd02dd6b30695218ab39f674349bc65ced34b
Rayonnant akasha <nyonsaakasha@gmail.com>
2025-07-27 18:48:23 +0100

    Braeking change: change structure for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import MesProjets from './pages/MesProjets'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ScrollToTopButton from './components/ScrollToTopButton'
 import "./styles/App.css"
 import "./styles/index.css"
 
@@ -9,9 +13,16 @@ function App() {
 
   return (
     <>
+      <Header />
+      <div className="lg:pt-2 lg:pb-40 pt-24 pb-32"/>
+      <main>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projets" element={<MesProjets />} />
       </Routes>
+      </main>
+      <Footer />
+      <ScrollToTopButton />
     </>
   )
 }
